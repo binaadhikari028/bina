bc3824ef4e997ee5d5c116d55e54154db5805302
Bina Adhikari <binaadhikari028@gmail.com>
2026-01-24 13:21:14 +0545

    week12 Done

---
+++ week12/WarmUp.java
@@ -0,0 +1,24 @@
+package week12;
+import java.util.ArrayList;
+
+public class WarmUp
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Sushil");
+        names.add("Anil");
+        names.add("Subash");
+        names.add("Kamal");
+        System.out.println(names);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        System.out.println(numbers);
+
+
+
+    }
+}
